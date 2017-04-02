5b4c64dc54210bd4c21a2cb23336f5cf501ff7d3
Drew Nuttall-Smith <drew@mysticpants.com>
2017-04-02 11:54:55 +1000

    Made sort alternate between axes. Stopped creating empty trees.

--- src/nearestNeigh/KDTreeNN.java
+++ src/nearestNeigh/KDTreeNN.java
@@ -16,6 +16,17 @@ public class KDTreeNN implements NearestNeigh{
     private KDTreeNN left = null;
     private KDTreeNN right = null;
 
+    // Axis to sort on
+    private char axis;
+
+    public void KDTreeNN() {
+        this.axis = 'x';
+    }
+
+    public void KDTreeNN(char axis) {
+        this.axis = axis;
+    }
+
     @Override
     public void buildIndex(List<Point> points) {
         // To be implemented.
@@ -25,33 +36,23 @@ public class KDTreeNN implements NearestNeigh{
         int median = ceil(points.size() / 2);
         point = points.get(middle);
 
-        // Build the left and right tree lists
-        left = new KDTreeNN();
-        right = new KDTreeNN();
-        left.buildIndex(points.subList(0, median));
-        right.buildIndex(points.subList(median + 1, points.size()));
+        // Get the sort axis for the next layer down
+        char nextAxis = (axis == 'x') ? 'y' : 'x';
 
-    }
-
-    /**
-     * @description sort the list of points using bubble sort
-     * @param points the list of points to sort
-     * @return List<Point> the sorted list of points
-     **/
-    public List<Point> sort(List<Point> points) {
-
-        // Sort the provided list
-        for (int i = 0; i < points.size(); i++) {
-            for (int j = 0; j < points.size() - 1; j++) {
-                if (points.get(j) > points.get(j+1)) {
-                    Point temp = points.get(j);
-                    points.set(j, points.get(j+1));
-                    points.set(j+1, temp);
-                }
-            }
+        // Build the left tree
+        List<Point> leftList = points.subList(0, median);
+        if (leftList.size() > 0) {
+            left = new KDTreeNN(nextAxis);
+            left.buildIndex(leftList);
         }
 
-        return points;
+        // Build the right tree
+        List<Point> rightList = points.subList(median + 1, points.size());
+        if (rightList.size() > 0) {
+            right = new KDTreeNN(nextAxis);
+            right.buildIndex(rightList);
+        }
+
     }
 
     @Override
@@ -76,6 +77,47 @@ public class KDTreeNN implements NearestNeigh{
     public boolean isPointIn(Point point) {
         // To be implemented.
         return false;
+    }
+
+    /* ---------- Private Methods ---------- */
+
+    /**
+     * @description sort the list of points using bubble sort
+     * @param points the list of points to sort
+     * @return List<Point> the sorted list of points
+     **/
+    private List<Point> sort(List<Point> points) {
+
+        if (axis == 'x') {
+
+            // Sort the provided list by latitude
+            for (int i = 0; i < points.size(); i++) {
+                for (int j = 0; j < points.size() - 1; j++) {
+                    if (points.get(j).lat > points.get(j+1).lat) {
+                        Point temp = points.get(j);
+                        points.set(j, points.get(j+1));
+                        points.set(j+1, temp);
+                    }
+                }
+            }
+
+        } else if (axis == 'y') {
+
+            // Sort the provided list by longitude
+            for (int i = 0; i < points.size(); i++) {
+                for (int j = 0; j < points.size() - 1; j++) {
+                    if (points.get(j).lon > points.get(j+1).lon) {
+                        Point temp = points.get(j);
+                        points.set(j, points.get(j+1));
+                        points.set(j+1, temp);
+                    }
+                }
+            }
+
+        }
+
+
+        return points;
     }
 
 }
