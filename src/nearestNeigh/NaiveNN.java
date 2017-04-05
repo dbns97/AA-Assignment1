3773a50a2f40c0dc0e271e4527ea84ec1b405469
shawnotaylor <shawnotaylor04@gmail.com>
2017-04-05 18:54:13 +1000

    finished naive
    did all the functions how i havent worked with List and ArrayList before
    so im not sure if i did it right
    Also havent tested it

--- src/nearestNeigh/NaiveNN.java
+++ src/nearestNeigh/NaiveNN.java
@@ -13,29 +13,74 @@ public class NaiveNN implements NearestNeigh{
     @Override
     public void buildIndex(List<Point> points) {
         // To be implemented.
+        list<Point> index = new List<Point>();
+        for ( int i = 0; i < points.size(); i++ )
+        {
+            index.add( points.get(i) ) ;
+        }
     }
 
     @Override
     public List<Point> search(Point searchTerm, int k) {
         // To be implemented.
-        return new ArrayList<Point>();
+        ArrayList<Point> closestPoints = new ArrayList<Point>();
+        double closestDist;
+
+        for ( int j = 0; j < k; j++)
+        {
+            closestDist = searchTerm.distTo( index<j> );
+            for(int i=0; i < index.size(); i++)
+            {
+                if( searchTerm.distTo( index<i> ) < closestDist )
+                {
+                    closestPoints.add( index<i> );
+                }
+            }
+        }
+        
+        return closestPoints;
     }
 
     @Override
     public boolean addPoint(Point point) {
         // To be implemented.
-        return false;
+        for(int i=0; i < index.size(); i++)
+        {
+            if( point.equals(index<i>) )
+            {
+                return false;
+            }
+        }
+        // if here means there isnt any points in index the same as point want to add
+        // can now add to index
+        index.add(point);
+        return true;
     }
 
     @Override
     public boolean deletePoint(Point point) {
         // To be implemented.
+        for(int i=0; i < index.size(); i++)
+        {
+            if( point.equals(index<i>) )
+            {
+                index.remove(i)
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
     public boolean isPointIn(Point point) {
         // To be implemented.
+        for(int i=0; i < index.size(); i++)
+        {
+            if( point.equals(index<i>) )
+            {
+                return true;
+            }
+        }
         return false;
     }
 
