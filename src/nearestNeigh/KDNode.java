c54d68da47d1a9aa7715c28b74e16545569de768
Drew Nuttall-Smith <drew@mysticpants.com>
2017-04-06 00:45:30 +1000

    Wrote initial version of build index
    Wrote sort function for build index
    Added getters and setters to KDNode

--- src/nearestNeigh/KDNode.java
+++ src/nearestNeigh/KDNode.java
@@ -1,17 +1,53 @@
 public class KDNode {
 
     // The point contained in this node
-    public Point point = null;
+    private Point point = null;
     // THe point to the left of this node
-    public Node left = null;
+    private Node left = null;
     // THe point to the right of this node
-    public Node right = null;
+    private Node right = null;
     // The axis to split on at this node
-    public Axis axis = null;
+    private Axis axis = null;
 
     // Constructor
     public KDNode(Point point, Axis axis) {
         this.point = point;
+        this.axis = axis;
+    }
+
+    /*---------- Getters ----------*/
+
+    public Point getPoint() {
+        return point;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+    public Axis getAxis() {
+        return axis;
+    }
+
+    /*---------- Setters ----------*/
+
+    public Point setPoint(Point point) {
+        this.point = point;
+    }
+
+    public Node setLeft(Node node) {
+        this.left = node;
+    }
+
+    public Node setRight(Node node) {
+        this.right = node;
+    }
+
+    public Axis setAxis(Axis axis) {
         this.axis = axis;
     }
 
--- src/nearestNeigh/KDTreeNN.java
+++ src/nearestNeigh/KDTreeNN.java
@@ -2,6 +2,7 @@ package nearestNeigh;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.lang.Math;
 
 /**
  * This class is required to be implemented.  Kd-tree implementation.
@@ -16,6 +17,31 @@ public class KDTreeNN implements NearestNeigh{
     @Override
     public void buildIndex(List<Point> points) {
         // To be implemented.
+
+        root = setNode(points, X);
+
+    }
+
+    private void setNode(List<Point> points, Axis axis) {
+        // Sort the list of points on the supplied axis
+        points = sort(points, axis);
+
+        // Find the median point and initialise the node
+        int median = floor(points.size() / 2);
+        KDNode node = new KDNode(points.get(median), axis);
+
+        // Set axis for next layer of the tree
+        Axis nextAxis = (axis == X) ? Y : X;
+
+        // Get lists of points for two sub trees
+        List<Point> leftPoints = points.subList(0, median);
+        List<Point> rightPoints = points.subList(median + 1, points.size());
+
+        // Set the left and right nodes
+        node.setLeft(setNode(leftPoints, nextAxis));
+        node.setRight(setNode(rightPoints, nextAxis));
+
+        return node;
     }
 
     @Override
@@ -40,6 +66,46 @@ public class KDTreeNN implements NearestNeigh{
     public boolean isPointIn(Point point) {
         // To be implemented.
         return false;
+    }
+
+    /* ---------- Private Methods ---------- */
+
+    /**
+     * @description sort the list of points using bubble sort
+     * @param points the list of points to sort
+     * @return List<Point> the sorted list of points
+     **/
+    private List<Point> sort(List<Point> points, Axis axis) {
+
+        if (axis == X) {
+
+            // Sort the provided list by latitude
+            for (int i = 0; i < points.size(); i++) {
+                for (int j = 0; j < points.size() - 1; j++) {
+                    if (points.get(j).lat > points.get(j+1).lat) {
+                        Point temp = points.get(j);
+                        points.set(j, points.get(j+1));
+                        points.set(j+1, temp);
+                    }
+                }
+            }
+
+        } else if (axis == Y) {
+
+            // Sort the provided list by longitude
+            for (int i = 0; i < points.size(); i++) {
+                for (int j = 0; j < points.size() - 1; j++) {
+                    if (points.get(j).lon > points.get(j+1).lon) {
+                        Point temp = points.get(j);
+                        points.set(j, points.get(j+1));
+                        points.set(j+1, temp);
+                    }
+                }
+            }
+
+        }
+
+        return points;
     }
 
 }
